0687c44b25193806e6026f6719dc5773b7a9cbee
Yan <126959161+yan4H@users.noreply.github.com>
2023-05-20 15:09:23 +0300

    Update lowest_common_ancestors.py

--- networkx/algorithms/lowest_common_ancestors.py
+++ networkx/algorithms/lowest_common_ancestors.py
@@ -10,7 +10,9 @@
 #
 # Author:  Alex Roper <aroper@umich.edu>
 """Algorithms for finding the lowest common ancestor of trees and DAGs."""
-from collections import defaultdict, Mapping, Set
+from collections import defaultdict
+from collections.abc import Mapping
+from builtins import set as Set
 from itertools import chain, count
 
 import networkx as nx
